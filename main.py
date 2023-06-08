1cfe32d7cfd94edbfaa4f941e822af0c9d4174f7
Tomin Joy <tominjk007@gmail.com>
2023-06-08 13:30:44 +0530

    cors update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Medlab backend",
 app.add_middleware(
 
     CORSMiddleware,
-    allow_origins=["*","http://localhost:3000 ","https://medlab-mini.netlify.app"],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
