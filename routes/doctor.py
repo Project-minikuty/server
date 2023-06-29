18dcb87287b613f4160c975404d156a769c5cca8
Tomin Joy <tominjk007@gmail.com>
2023-06-29 22:06:06 +0530

    [added] get appointment list

--- routes/doctor.py
+++ routes/doctor.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from bson import ObjectId
-from db import  c2j, get_db
+from db import c2j, get_db
 from datetime import date, datetime
 
 
@@ -9,21 +9,34 @@ app = APIRouter(
     tags=["Doctor"],
 )
 
+
 @app.get("/patients")
-def patient_list(username :str):
+def patient_list(username: str):
     d = db["doctors"]
-    p = d.find_one({"username":username},{"patients":1,"_id":0})
+    p = d.find_one({"username": username}, {"patients": 1, "_id": 0})
     return p["patients"] if len(p) else {}
 
-@app.get("/appointments")
-def appointment_list(username:str,date_ :str = str(datetime.now().date())):
-    d = db["doctors"]
-    p = d.find_one({"username":username},{f"appointments.{date_}":1,"_id":0})
-    return p["appointments"].get(date_,[]) if len(p) else {}
+
+@app.get("/dappointments")
+def doctors_appointment_list(username: str,aType :str ,date_: str = str(datetime.now().date())):
+    d = db[aType]
+    p = d.find({
+        "doc": username,
+        "date": date_
+    }, {"_id": 0})
+    return c2j(p)
+@app.get("/pappointments")
+def patients_appointment_list(username: str,aType :str ,date_: str = str(datetime.now().date())):
+    d = db[aType]
+    p = d.find({
+        "pat": username,
+        "date": date_
+    }, {"_id": 0})
+    return c2j(p)
+    
+
 
 @app.post("/cPrescription")
 def create_Prescription():
 
     return {}
-
-
